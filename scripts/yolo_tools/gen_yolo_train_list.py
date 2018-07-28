4f050caa5bc503bdc361c8d40646cf583007ba74
Yu-Chen Xue <32350820+SeanSyue@users.noreply.github.com>
2018-07-28 13:28:31 +0800

    Create gen_yolo_train_list.py

---
+++ scripts/yolo_tools/gen_yolo_train_list.py
@@ -0,0 +1,13 @@
+"""
+Generate list of absolute path to labelled images for yolo training.
+Usage:
+Rename the folder containing labelled images as 'JPEGImages'
+and the corresponding label file folder as `labels`.
+Put this script and two folders above under the same directory in a yolo training environment,
+then run this script to generate training list.
+"""
+from pathlib import Path
+
+for img_file in Path('/JPEGImages').iterdir():
+    with open('defect_train.txt', 'w') as f:
+        print(img_file.resolve(), file=f)
