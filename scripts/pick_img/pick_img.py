9a734b187cfc5aaa139bcc942d2e6a5e980ef4c9
SeanSyue <sean962081468@gmail.com>
2018-09-07 15:14:21 +0800

    some renaming and moving

--- scripts/pick_img/pick_img.py
+++ scripts/pick_img/pick_img.py
@@ -15,9 +15,10 @@ def sorted_alphanumeric(data):
 
 
 if __name__ == '__main__':
+    # first sort images with alphanumeric order
     for item in sorted_alphanumeric(listdir(f'data/Labels/{PATH_NUM}'))[:SLICE_NUM]:
         with open(joinpath(f'./data/Labels/{PATH_NUM}', item)) as f:
-            if f.readline().strip('\n') != '0':
+            if f.readline().strip('\n') != '0':  # exclude images and labels that with no bounding boxes
                 copyfile(joinpath(f'data/Images/{PATH_NUM}', item.replace('txt', 'bmp')),
                          joinpath('data/defect_yolo_center/JPEGImage', item.replace('txt', 'bmp')))
                 copyfile(joinpath(f'data/Labels/{PATH_NUM}', item),
